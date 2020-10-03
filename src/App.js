246b4d186849261580026d788eaebb010ec680ea
MahmudJion <43998247+MahmudJion@users.noreply.github.com>
2020-10-03 22:03:56 +0600

    init setup with bubble mode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
+import Particles from 'react-particles-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+    // particles: {
+    //     number: {
+    //         value: 250,
+    //         density: {
+    //             enable: true,
+    //             value_area: 1500
+    //         }
+    //     }
+    // }
+}
+
+function App () {
+    return (
+        // <div className="App">
+        //     <Particles className='particles'
+        //         params={particlesOptions}
+        //     />
+        // </div>
+        <Particles
+            params={{
+                "particles": {
+                    "number": {
+                        "value": 250,
+                        "density": {
+                            "enable": false
+                        }
+                    },
+                    "size": {
+                        "value": 3,
+                        "random": true,
+                        "anim": {
+                            "speed": 5,
+                            "size_min": 0.5
+                        }
+                    },
+                    "line_linked": {
+                        "enable": false
+                    },
+                    "move": {
+                        "random": true,
+                        "speed": 1,
+                        "direction": "top",
+                        "out_mode": "out"
+                    }
+                },
+                "interactivity": {
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "bubble"
+                        },
+                        "onclick": {
+                            "enable": true,
+                            "mode": "repulse"
+                        }
+                    },
+                    "modes": {
+                        "bubble": {
+                            "distance": 250,
+                            "duration": 2,
+                            "size": 0,
+                            "opacity": 0
+                        },
+                        "repulse": {
+                            "distance": 400,
+                            "duration": 4
+                        }
+                    }
+                }
+            }} />
+    )
 }
 
 export default App;
